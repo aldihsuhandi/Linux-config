8d895dc578a6787c294d4ef19d954ddd6a293f4b
aldihsuhandi <aldihsuhandi697@gmail.com>
2020-04-29 10:33:40 +0700

    .

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 # !/bin/bash
 
 #setting up .vimrc and .bashrc
+echo 'setting up bash and vim'
 cp ./bash/.bashrc ~/.bashrc
 cp ./vim/.vimrc ~/.vimrc
 bash
+echo 'finish setting up bash and vim'
 
 #running script for yay
 ./script/yay.sh
+
