9c61f2cb98cf5f894e899aa65f8690aae093f0ae
aldihsuhandi <aldihsuhandi697@gmail.com>
2020-04-29 19:23:23 +0700

    .

---
+++ script/powerline.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+
+echo "installing powerline-shell"
+sudo pacman -S pip-python
+sudo pip install powerline-shell
+
+mkdir -p ~/.config/powerline-shell && powerline-shell --generate-config > ~/.config/powerline-shell/config.json
+
+cp ../bash/config.json ~/.config/powerline-shell/config.json
+echo "finish installing powerline-shell"
--- script/vim.sh
+++ script/vim.sh
@@ -1,5 +1,4 @@
 # !/bin/bash
 cd  ~
-pwd
 mkdir ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/plugManager/Vundle.vim
