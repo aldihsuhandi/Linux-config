88f7b78274c56215aeee2563cda9059dfec97fa1
aldihsuhandi <aldihsuhandi697@gmail.com>
2020-04-29 10:58:20 +0700

    .

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 # !/bin/bash
 
-#setting up .vimrc and .bashrc
-echo 'setting up bash and vim'
-cp ./bash/.bashrc ~/.bashrc
-cp ./vim/.vimrc ~/.vimrc
-./script/vim.sh
-bash
-echo 'finish setting up bash and vim'
+echo "only copy vimrc and bashrc? [Y/N]"
+read inp
+if [ $inp = "Y" -o $inp = "y" ]
+then
+    echo "copying .vimrc and .bashrc to home..."
+    cp ./bash/.bashrc ~/.bashrc
+    cp ./vim/.vimrc ~/.vimrc
+    bash
+    echo "Finish"
+elif [ $inp = "N" -o $inp = "n" ]
+then
+    # setting up .vimrc and .bashrc
+    echo 'setting up bash and vim'
+    cp ./bash/.bashrc ~/.bashrc
+    cp ./vim/.vimrc ~/.vimrc
+    ./script/vim.sh
+    bash
+    echo 'finish setting up bash and vim'
 
-#running script for yay
-./script/yay.sh
-
+    # running script for yay
+    ./script/yay.sh
+else
+    echo 'there is nothing to do..'
+fi
