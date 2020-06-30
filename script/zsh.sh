0d55d026c18f0fa53bbb2113aa083a3a12ea137d
aldih <aldihsuhandi697@gmail.com>
2020-06-30 07:13:20 +0700

    .

--- script/zsh.sh
+++ script/zsh.sh
@@ -1,9 +1,10 @@
 # !/bin/bash
 
 sudo pacman -S zsh
-cp ./zsh/.zshrc ~/.zshrc
+cp ../zsh/.zshrc ~/.zshrc
 
 cd ~
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+exit
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
