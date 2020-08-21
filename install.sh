363bfb6e4ef35e8816735f69ab615fff0ab839ca
aldih <aldihsuhandi697@gmail.com>
2020-08-21 23:53:18 +0700

    adding bitwarden to the preinstalled package

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ then
     # installing package from repo
     echo 'installing various applications (press ctrl + c to cancel")'
     echo ' '
-    yay -S discord brave spotify alacritty exa audacious minetime-bin playerctl alsa winetricks firefox kdeconnect neofetch ripgrep noto-fonts-emoji noto-fonts-cjk mailspring gnome-keyring libgnome-keyring unrar
+    yay -S discord brave spotify alacritty exa audacious minetime-bin playerctl alsa winetricks firefox kdeconnect neofetch ripgrep noto-fonts-emoji noto-fonts-cjk mailspring gnome-keyring libgnome-keyring unrar bitwarden-bin
     
     echo 'setting up alacritty'
     echo ' '
